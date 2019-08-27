6288dcd33f8f30657319eef76839edf6082ae63e
Carlos Ayala <cayalav17@gmail.com>
2019-08-27 01:12:46 -0500

    Implementacion con primera imagen

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,20 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <div>
+    
+    <div className="Hero">
+      
+      <div className="HeroGroup">
+      <h1>Hey! Hola,<br/> mi nombre es Carlos</h1>
+      <p>Te invito a ver mi sitio web y que puedas ver uno de mis trabajos</p>
+      <Link to="/video">"Click para conocerme mejor"</Link>
+      
+      </div>
+      
+
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </div>
 )
 
 export default IndexPage
